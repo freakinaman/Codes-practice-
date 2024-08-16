021b32157f6423708c21be7e7d876390163cbe0c
freakinaman <98891116+freakinaman@users.noreply.github.com>
2024-08-16 22:52:04 +0530

    Time: 7 ms (82.70%), Space: 64.3 MB (77.64%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
@@ -1,30 +1,22 @@
 class Solution {
-    public int maxDistance(List<List<Integer>> arr) {
+    public int maxDistance(List<List<Integer>> arrays) {
+        int globalMin = arrays.get(0).get(0);
+        int globalMax = arrays.get(0).get(arrays.get(0).size() - 1);
+        int maxDistance = 0;
 
-        int maxDiff = Integer.MIN_VALUE;
-        
-        int small = arr.get(0).get(0);
-        int larg = arr.get(0).get(arr.get(0).size()-1);
+        for (int i = 1; i < arrays.size(); i++) {
+            int currentMin = arrays.get(i).get(0);
+            int currentMax = arrays.get(i).get(arrays.get(i).size() - 1);
 
+            // Calculate max distance with current array
+            maxDistance = Math.max(maxDistance, Math.abs(currentMax - globalMin));
+            maxDistance = Math.max(maxDistance, Math.abs(globalMax - currentMin));
 
-        System.out.println(small + "   " + larg);
-        
-        for(int i =1 ; i<arr.size() ; i++){
-
-            int fd = Math.abs(arr.get(i).get(arr.get(i).size()-1) - small );
-            int ld = Math.abs(larg -arr.get(i).get(0) );
-
-            int d=(fd>ld)?fd:ld; 
-
-            maxDiff = Math.max(maxDiff,d);
-
-            small = Math.min(arr.get(i).get(0) , small);
-            larg = Math.max(larg ,arr.get(i).get(arr.get(i).size()-1) );
-
+            // Update global min and max
+            globalMin = Math.min(globalMin, currentMin);
+            globalMax = Math.max(globalMax, currentMax);
         }
-        
-        
-        return maxDiff;
-        
+
+        return maxDistance;
     }
 }
