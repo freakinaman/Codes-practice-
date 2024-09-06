d1e29206954ddd014fe2da3a1cb1fe027066cfe4
Aman Sharma <amanpiyushsharma@gmail.com>
2024-09-06 22:25:35 +0530

    Time: 2 ms (63.56%), Space: 40.7 MB (96.80%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+        ArrayList<Integer> nums=new ArrayList<>();
+        int fact=1;
+        for(int i=1;i<n;i++){
+            nums.add(i);
+            fact=fact*i;
+        }
+        nums.add(n);
+        String ans="";
+        k=k-1;
+        while(true){
+            ans=ans+nums.get(k/fact);
+            nums.remove(k/fact);
+            if(nums.size()==0){
+                break;
+            }
+            k=k%fact;
+            fact=fact/nums.size();
+        }
+        return ans;
+    }
+}
