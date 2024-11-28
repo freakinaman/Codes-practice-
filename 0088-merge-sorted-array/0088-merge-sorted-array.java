5506451b649066a2cfab74fd166542daaf860c99
Aman Sharma <amanpiyushsharma@gmail.com>
2024-11-29 01:30:12 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (18.57%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -5,7 +5,7 @@ class Solution {
         int k = m + n - 1;
         
         while (j >= 0) {
-            if (i>=0 &&nums1[i] > nums2[j]) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
                 nums1[k--] = nums1[i--];
             } else {
                 nums1[k--] = nums2[j--];
